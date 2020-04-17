54e57e39ff83419734a70dd49ea6e8569516a5b1
Sandeep <sunthapa1@hotmail.com>
2020-04-18 00:52:33 +0100

    server can now handel each request in parallel

--- posenetServer/appClusterFaster.js
+++ posenetServer/appClusterFaster.js
@@ -1,18 +1,8 @@
-var cluster = require('cluster')
 var numCPUs = require('os').cpus().length;
-const tf = require('@tensorflow/tfjs-node');
-const posenet = require('@tensorflow-models/posenet');
+
 var express = require('express');
 var bodyParser = require('body-parser');
-var pixels = require('image-pixels');
-var async = require("async");
-
-
-var boundingBoxes = [];
-var poseDetectionResults = {}
-const {
-    createCanvas, Image
-} = require('canvas')
+var workerpool = require('workerpool');
 
 var app = express();
 app.use(bodyParser.urlencoded({limit: '10mb', extended: true}))
@@ -22,22 +12,7 @@ app.get('/getImage',function(req,res){
     res.send(data);
 
 })
-        
 
-        
-loadModel = async function(req,res){
-    console.log("Loading model");
-    net = await posenet.load({
-        architecture: 'ResNet50',
-        outputStride: 32,
-        inputResolution: { width: 300 , height: 300 },
-        quantBytes: 4,
-        multiplier:1
-        });
-    console.log("Model Loaded!");
-    
-}
-loadModel()
 
 // app.get('/loadModel', async function(req,res){
 //     console.log("Loading model");
@@ -53,23 +28,32 @@ loadModel()
     
 // })
 
+var pool = workerpool.pool(__dirname + '/workerThread.js' );
+
+var loadWorkersModel = async ()=>{
+    var promises = Array(7).fill().map((_,index)=>{
+        return pool.exec('loadModel')
+    })
+    //console.log("Loading Models In All Workers!")
+    var res = await Promise.all(promises)
+    //console.log("Model Loaded In All Workers!")
+
+}
+
+loadWorkersModel()
 
 app.post('/postImage' ,async function(req,res){
     const imgBase64List = req.body.imgBase64.split('next');
     const imgBase64IndexList = []
     imgBase64List.map((imgBase64,index)=>{
-        imgBase64IndexList.push([index,imgBase64])
+        imgBase64IndexList.push([index+'index'+imgBase64])
     })
-    async.each(imgBase64IndexList , getPoseDetection , function(err){
-        if(err){
-            console.log("Error occured when feteching one of the pose")
-            console.log(err)       
-        }else{
-            console.log('Sending Poses!')
-            res.send(JSON.stringify(poseDetectionResults))
-        }
+    var promises = Array(imgBase64IndexList.length).fill().map((_,index)=>{
         
+        return pool.exec('threadFunction',imgBase64IndexList[index])
     })
+    const segmentsResults = await Promise.all(promises);
+    res.send(JSON.stringify({detection:segmentsResults}))
         
     
 
@@ -81,42 +65,13 @@ app.post('/getBBox' ,async function(req,res){
 });//End of app.post and asyn function
 
 
-
-const getPoseDetection = async (imgBase64Index, callback) => {
-    //console.log('start');
-
-    const img = new Image();
-    img.src = imgBase64Index[1];
-    const imgIndex = imgBase64Index[0]
-
-
-    const canvas = createCanvas(img.width, img.height);
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(img, 0, 0);
-    const input = tf.browser.fromPixels(canvas);
-    const imageScaleFactor = 0.3;
-    const flipHorizontal = false;
-    const outputStride = 8;
-
-    const poses = await net.estimateMultiplePoses(input, {
-        flipHorizontal: false,
-        maxDetections: 2,
-        minPoseConfidence: 0.15,
-        minPartConfidence:0.1,
-        nmsRadius:20,
-        });
-    
-    boundingBoxes = [] //reset the boundingBoxese
-    poses.forEach(pose => {
-        var box = posenet.getBoundingBoxPoints(pose.keypoints);
-        boundingBoxes.push(box)
-    });  
-  
-    poseDetectionResults[imgIndex] = {detectionList:poses}
-    //console.log(poses)
-
-}
 app.listen(3000);
+
+
+
+
+
+
 
 
 
